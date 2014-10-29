34bacf8854a04ac4600ef8f2581ea67416ad22d8
Igor Pashev <pashev.igor@gmail.com>
2014-10-29 20:55:34 +0300

    Allow GHC 7.8

--- src/Replicator.hs
+++ src/Replicator.hs
@@ -53,6 +53,7 @@ Options:|]
 defineFlag "f:force" False "Force action, e. g. overwrite dumps"
 defineFlag "a:all" False "Act on all channels"
 defineFlag "c:config" "channels.ini" "Path to configuration file"
+$(return []) -- GHC 7.8 https://github.com/errge/hflags/issues/8
 
 type Action = Cf.ConfigParser -> Cf.SectionSpec -> IO Cf.ConfigParser
 
