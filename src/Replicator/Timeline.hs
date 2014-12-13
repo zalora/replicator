4160f5fffbbe94651c1c7ce73fdfc82b2cc8f5ad
Igor Pashev <pashev.igor@gmail.com>
2014-12-13 16:30:16 +0300

    Renamed argument

--- src/Replicator/Timeline.hs
+++ src/Replicator/Timeline.hs
@@ -12,9 +12,9 @@ seconds = fmap (round' . realToFrac) getPOSIXTime where
     round' = round :: Double -> Integer
 
 timestamp :: Integer -> String -> IO ()
-timestamp start msg = do
+timestamp zero msg = do
     t <- seconds
-    putStrLn $ mark (t - start) ++ " " ++ msg
+    putStrLn $ mark (t - zero) ++ " " ++ msg
     where mark s = printf "%02u:%02u:%02u" h m' s' where
             (m, s') = s `divMod` 60
             (h, m') = m `divMod` 60
