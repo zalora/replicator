60bedc89b82420edadb2a820d5407a07139e09be
Igor Pashev <pashev.igor@gmail.com>
2015-01-20 15:28:58 +0800

    None or "auto"

--- src/Replicator/Config.hs
+++ src/Replicator/Config.hs
@@ -45,7 +45,7 @@ buildOption :: Cf.ConfigParser
             -> (Cf.OptionSpec,  Cf.ConfigParser -> Cf.SectionSpec -> String)
             -> Cf.ConfigParser
 buildOption conf (opt, builder) = foldl go conf (Cf.sections conf) where
-    go cf s = if Cf.has_option cf s opt then cf
+    go cf s = if Cf.has_option cf s opt && ("auto" /= get cf s opt) then cf
               else set cf s opt (builder cf s)
 
 addOptions :: Cf.ConfigParser -> Cf.ConfigParser
